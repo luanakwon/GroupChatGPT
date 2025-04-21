ec02dcbcf70a3cb8dba7578ec090ffec68d3c7a3
luanakwon <junnyboy99@gmail.com>
2025-04-21 15:56:22 -0700

    implemented logging

--- bot/main.py
+++ bot/main.py
@@ -8,11 +8,6 @@ from bot.discord.discord_client import MyDiscordClient
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
 
-    try:
-        1 / 0
-    except Exception:
-        logger.error("ZeroDivision!", exc_info=True)
-
     client = MyDiscordClient()
     client.run(DISCORD_TOKEN, log_handler=None)
     
