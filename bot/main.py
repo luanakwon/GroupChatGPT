eb102f9e579dedc427cfa193521d022727b4cd9f
luanakwon <junnyboy99@gmail.com>
2025-05-18 10:34:48 -0700

    instantiate clients & set reference attributes in main

--- bot/main.py
+++ bot/main.py
@@ -2,13 +2,29 @@ import logging
 import bot.config.mylogger as mylogger
 mylogger.set_logging()
 
-from bot.config.credentials import DISCORD_TOKEN
+from bot.config.credentials import DISCORD_TOKEN, OPENAI_API_KEY
+from bot.db.vector_db import Topic_VDB
 from bot.discord.discord_client import MyDiscordClient
+from bot.llm.llm_client import MyOpenAIClient
+from bot.llm.llm_rag import LLM_RAG
 
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
 
-    client = MyDiscordClient()
-    client.run(DISCORD_TOKEN, log_handler=None)
+    # instantiate clients
+    discordClient = MyDiscordClient()
+    topicVDB = Topic_VDB(persist_directory='./VDB_persist')
+    openaiClient = MyOpenAIClient(api_key = OPENAI_API_KEY)
+    RAG_module = LLM_RAG(retrieval_limit=100)
+
+    # set reference attributes
+    discordClient.RAG = RAG_module
+    topicVDB.LLMClient = openaiClient
+    RAG_module.DB = topicVDB
+    RAG_module.DiscordClient = discordClient
+    RAG_module.LLMClient = openaiClient
+    
+    discordClient.run(DISCORD_TOKEN, log_handler=None)
+    
     
     
