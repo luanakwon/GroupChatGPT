6e61263e03473959f4c8666c79bdbea168d6203e
luanakwon <junnyboy99@gmail.com>
2025-05-19 11:50:32 -0700

    changed Client to persistentClient

--- bot/db/vector_db.py
+++ bot/db/vector_db.py
@@ -3,7 +3,9 @@ from typing import TYPE_CHECKING
 
 import chromadb
 from chromadb.config import Settings
+from chromadb import Documents, EmbeddingFunction, Embeddings
 
+import chromadb.errors
 import numpy as np
 import uuid
 from datetime import datetime
@@ -12,36 +14,42 @@ if TYPE_CHECKING:
     from bot.llm.llm_client import MyOpenAIClient
 
 
+class MyEmbeddingFunction(EmbeddingFunction):
+    def set_openai_client(self,LLMClient:MyOpenAIClient):
+        self.LLMClient = LLMClient
+    def __call__(self, texts: Documents) -> Embeddings:
+        return self.LLMClient.get_embedding(texts)
+
 # my Topic DB based on ChromaDB
 class Topic_VDB:
     def __init__(self, persist_directory = "."):
         # ChromaDB Client
-        self.client = chromadb.Client(Settings(
-            persist_directory=persist_directory,
-            anonymized_telemetry=False
-        ))
+        self.client = chromadb.PersistentClient(
+            path=persist_directory,
+            settings=Settings(anonymized_telemetry=False)
+        )
         # cosine similarity threshold - bigger than this thres == same topic
         self.same_topic_threshold = 0.7
         # period for Moving Average when combining embeddings
         self.MA_period = 10
         
-        # Module reference
-        self.LLMClient:MyOpenAIClient = None
+    def set_llm_client(self, LLMClient):
+        self.embedding_func = MyEmbeddingFunction()
+        self.embedding_func.set_openai_client(LLMClient=LLMClient)
 
     # push an array of data to the channel's collection
     def push(self,channel_id,documents,timestamps):
         # create/get collection (distance=cosine)
         collection = self.client.get_or_create_collection(
             name=f"discord_topics_{channel_id}",
-            embedding_function=self.LLMClient.get_embedding,
+            embedding_function=self.embedding_func,
             configuration={'hnsw':{'space':'cosine'}}
         )
         # loop each document
+        embeddings = self.embedding_func(documents)
         document: str
         timestamp: datetime
-        for document, timestamp in zip(documents,timestamps):
-            # embed the document
-            embedding = self.LLMClient.get_embedding(document)
+        for document, embedding, timestamp in zip(documents,embeddings,timestamps):
             # query top k similar records
             result = collection.query(
                 query_embeddings=embedding,
@@ -49,21 +57,27 @@ class Topic_VDB:
                 include=['embeddings','metadatas','distances']
             )
             no_topics_match = True
-            for i in range(3):
+            for res_i, res_d, res_e, res_m in zip(
+                    result['ids'][0],
+                    result['distances'][0],
+                    result['embeddings'][0],
+                    result['metadatas'][0]):
                 # if any of the record are similar enough, combine into the existing topic
-                if result['distances'][i] < self.same_topic_threshold:
+                print(res_d)
+                if res_d > self.same_topic_threshold:
                     no_topics_match = False
                     # concat timestamp
-                    new_timestamps = result['metadatas'][i]['timestamps'] + ','+ timestamp.isoformat(timespec='seconds')
+                    new_timestamps = res_m['timestamps'] + ','+ timestamp.isoformat(timespec='seconds')
                     # combine embedding (Moving average & norm)
-                    new_embedding = (result['embeddings'][i] * self.MA_period + embedding)/(self.MA_period+1)
+                    new_embedding = (res_e * self.MA_period + embedding)/(self.MA_period+1)
                     new_embedding /= np.linalg.norm(new_embedding) if sum(new_embedding) > 0 else 1
                     # update collection
                     collection.update(
-                        ids=result['ids'][i],
+                        ids=res_i,
                         embeddings=new_embedding,
                         metadatas={'timestamps':new_timestamps}
                     )
+                    print(f"\n\n\nupdated collection {document}\n\n\n")
                 else:
                     # they are in distance-ascending order, 
                     # so escape once distance is too far
@@ -77,26 +91,33 @@ class Topic_VDB:
                     embeddings=[embedding],
                     metadatas=[{'timestamps':timestamp.isoformat(timespec='seconds')}],
                 )
+                print(f"\n\n\nadded to collection {document}\n\n\n")
 
     def query(self, channel_id, query_texts, k):
-        # get collection for the channel id
-        collection = self.client.get_collection(
-            name=f"discord_topics_{channel_id}",
-            embedding_function=self.LLMClient.get_embedding
-        )
-        # query top k results
-        result = collection.query(
-            query_texts=query_texts,
-            n_results=k,
-            include=['metadatas', 'distances']
-        )
-        
-        # convert string into list of datetimes
-        out_timestamps = [
-            [datetime.fromisoformat(_t) for _t in meta['timestamps'].split(',')]
-            for i, meta in enumerate(result['metadatas']) 
-            if result['distances'][i] > self.same_topic_threshold
-        ]
+        try:
+            # get collection for the channel id
+            collection = self.client.get_collection(
+                name=f"discord_topics_{channel_id}",
+                embedding_function=self.embedding_func
+            )
+            # query top k results
+            result = collection.query(
+                query_texts=query_texts,
+                n_results=k,
+                include=['metadatas', 'distances']
+            )
+            
+            print(result['distances'][0])
+
+            # convert string into list of datetimes
+            out_timestamps = [
+                [datetime.fromisoformat(_t) for _t in meta['timestamps'].split(',')]
+                for i, meta in enumerate(result['metadatas'][0]) 
+                if result['distances'][0][i] > self.same_topic_threshold
+            ]
+        except chromadb.errors.NotFoundError as e:
+            out_timestamps = []
+
         out = []
         for tlist in out_timestamps:
             out += sorted(tlist,reverse=True)
